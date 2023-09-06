154fc531fba54b948315b6c5ef843cfb887931c5
rn-mnsl <manansalarin@gmail.com>
2023-09-07 02:30:47 +0800

    DAY 15

--- day-15/day-15-project.py
+++ day-15/day-15-project.py
@@ -76,4 +76,3 @@ while is_on:
           payment = process_coins()
           if is_transaction_successful(payment, drink["cost"]): 
               make_coffee(response, drink["ingredients"])
-              
