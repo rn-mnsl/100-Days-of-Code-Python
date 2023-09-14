e936f2cb1b526e7ad368514755de4c7125b00c44
rn-mnsl <manansalarin@gmail.com>
2023-09-15 03:36:51 +0800

    day 21

--- day-21/main.py
+++ day-21/main.py
@@ -1,23 +1,75 @@
-class Animal: 
-    def __init__(self):
-        self.num_eyes = 2 
+from turtle import Turtle, Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("The Ssnek Game")
+screen.tracer(0)
+# segment_1 = Turtle("square")
+# segment_1.color("white")
+# segment_1.goto(0, 0)
+
+# segment_2 = Turtle("square")
+# segment_2.color("white")
+# segment_2.goto(-20, 0)
+
+# segment_3 = Turtle("square")
+# segment_3.color("white")
+# segment_3.goto(-40, 0)
+
+# easier solution 
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True 
+while game_is_on: 
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    #Detect collision with food. 
+    if snake.head.distance(food) < 15: 
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
     
-    def breathe(self):
-        print("Inhale, exhale.")
+    #Detect collision with wall. 
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
+    #Detect collition with tail 
+    for segment in snake.segments[1:]: 
+        if snake.head.distance(segment) < 10:
+            game_is_on = False 
+            scoreboard.game_over()
 
-class Fish(Animal): 
-    def __init__(self):
-        super().__init__()
+screen.exitonclick()
+
+      
     
-    def breathe(self):
-        super().breathe()
-        print("doing this underwater")
 
-    def swim(self): 
-        print("moving in water.")
+        
+        
 
-nemo = Fish()
-nemo.swim()
-nemo.breathe()
-print(nemo.num_eyes)
+
+
+
+
+
+
+
+screen.exitonclick()
